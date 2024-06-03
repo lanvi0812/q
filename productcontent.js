8e3c377b38ac22e14417ae220cb98e1b9aa994c7
lanvi0812 <161278764+lanvi0812@users.noreply.github.com>
2024-06-03 19:32:45 +1000

    Add files via upload

---
+++ productcontent.js
@@ -0,0 +1,126 @@
+const productsContent = [
+    {
+        "id": 1,
+        "name": "Small Bongo Blue",
+        "price": 60,
+        "description": "A petite ceramic wonder handcrafted with artisanal finesse in the vibrant heart of Melbourne. This small-sized marvel exudes elegance in its simplicity, boasting a calming blue hue that captivates the eye and soothes the soul. Meticulously crafted by skilled hands, each Bongo piece is a testament to the timeless artistry of ceramics, embodying a harmonious blend of form and function.",
+        "image": "images/product_bongoblue.png"
+    },
+
+    {  
+        "id": 2,
+        "name": "Small Bongo Pink",
+        "price": 60,
+        "description": "A petite ceramic wonder handcrafted with artisanal finesse in the vibrant heart of Melbourne. This small-sized marvel exudes elegance in its simplicity, boasting a calming blue hue that captivates the eye and soothes the soul. Meticulously crafted by skilled hands, each Bongo piece is a testament to the timeless artistry of ceramics, embodying a harmonious blend of form and function.",
+        "image": "images/product_bongopink.png"
+    },
+
+    {   
+        "id": 3,
+        "name": "Small Bongo Yellow",
+        "price": 60,
+        "description": "A petite ceramic wonder handcrafted with artisanal finesse in the vibrant heart of Melbourne. This small-sized marvel exudes elegance in its simplicity, boasting a calming blue hue that captivates the eye and soothes the soul. Meticulously crafted by skilled hands, each Bongo piece is a testament to the timeless artistry of ceramics, embodying a harmonious blend of form and function.",
+        "image": "images/product_bongoyellow.png"
+    },
+
+    {  
+        "id": 4,
+        "name": "Small Bongo Green",
+        "price": 60,
+        "description": "A petite ceramic wonder handcrafted with artisanal finesse in the vibrant heart of Melbourne. This small-sized marvel exudes elegance in its simplicity, boasting a calming blue hue that captivates the eye and soothes the soul. Meticulously crafted by skilled hands, each Bongo piece is a testament to the timeless artistry of ceramics, embodying a harmonious blend of form and function.",
+        "image": "images/product_bongogreen.png"
+    },
+
+    {  
+        "id": 5,
+        "name": "Medium Bongo Blue",
+        "price": 65,
+        "description": "A ceramic masterpiece meticulously crafted in the bustling artistic hub of Melbourne. This medium-sized marvel embodies the essence of artisanal excellence, showcasing a serene blue hue that enchants the beholder. Handmade with precision and care, each Bongo piece exudes a sense of refined elegance, elevating any space with its understated charm.",
+        "image": "images/product_bongomedblue.png"
+    },
+
+    {
+        "id": 6,
+        "name": "Medium Bongo Pink",
+        "price": 65,
+        "description": "A ceramic masterpiece meticulously crafted in the bustling artistic hub of Melbourne. This medium-sized marvel embodies the essence of artisanal excellence, showcasing a serene blue hue that enchants the beholder. Handmade with precision and care, each Bongo piece exudes a sense of refined elegance, elevating any space with its understated charm.",
+        "image": "images/product_bongomedpink.png"
+    },
+
+    {
+        "id": 7,
+        "name": "Medium Bongo Yellow",
+        "price": 65,
+        "description": "A ceramic masterpiece meticulously crafted in the bustling artistic hub of Melbourne. This medium-sized marvel embodies the essence of artisanal excellence, showcasing a serene blue hue that enchants the beholder. Handmade with precision and care, each Bongo piece exudes a sense of refined elegance, elevating any space with its understated charm.",
+        "image": "images/product_bongomedyellow.png"
+    },
+
+    {
+        "id": 8,
+        "name": "Medium Bongo Green",
+        "price": 65,
+        "description": "A ceramic masterpiece meticulously crafted in the bustling artistic hub of Melbourne. This medium-sized marvel embodies the essence of artisanal excellence, showcasing a serene blue hue that enchants the beholder. Handmade with precision and care, each Bongo piece exudes a sense of refined elegance, elevating any space with its understated charm.",
+        "image": "images/product_bongomedgreen.png"
+    },
+
+    {
+        "id": 9,
+        "name": "Large Bongo Blue",
+        "price": 72,
+        "description": "Meticulously crafted by skilled hands, each Bongo piece is a testament to the timeless artistry of ceramics, embodying a harmonious blend of form and function. Whether adorning a tabletop or serving as a cherished keepsake, Bongo brings a touch of refined beauty to any space, inviting admiration for its Melbourne-born craftsmanship and enchanting aura.",
+        "image": "images/product_bongolargeblue.png"
+    },
+
+    {
+        "id": 10,
+        "name": "Large Bongo Pink",
+        "price": 72,
+        "description": "Meticulously crafted by skilled hands, each Bongo piece is a testament to the timeless artistry of ceramics, embodying a harmonious blend of form and function. Whether adorning a tabletop or serving as a cherished keepsake, Bongo brings a touch of refined beauty to any space, inviting admiration for its Melbourne-born craftsmanship and enchanting aura.",
+        "image": "images/product_bongolargepink.png"
+    },
+
+    {
+        "id": 11,
+        "name": "Large Bongo Yellow",
+        "price": 72,
+        "description": "Meticulously crafted by skilled hands, each Bongo piece is a testament to the timeless artistry of ceramics, embodying a harmonious blend of form and function. Whether adorning a tabletop or serving as a cherished keepsake, Bongo brings a touch of refined beauty to any space, inviting admiration for its Melbourne-born craftsmanship and enchanting aura.",
+        "image": "images/product_bongolargeyellow.png"
+    },
+
+    {
+        "id": 12,
+        "name": "Large Bongo Green",
+        "price": 72,
+        "description": "Meticulously crafted by skilled hands, each Bongo piece is a testament to the timeless artistry of ceramics, embodying a harmonious blend of form and function. Whether adorning a tabletop or serving as a cherished keepsake, Bongo brings a touch of refined beauty to any space, inviting admiration for its Melbourne-born craftsmanship and enchanting aura.",
+        "image": "images/product_bongolargegreen.png"
+    },
+
+    {
+        "id": 16,
+        "name": "Oval Potion Pot",
+        "price": 80,
+        "description": "Each Potion is a testament to Melbourne's rich artistic tradition, meticulously formed by skilled hands to embody both beauty and functionality. Whether displayed as a centerpiece or used to adorn a sacred space, Potion exudes an aura of magic and allure, inviting admirers to immerse themselves in its Melbourne-made charm and timeless elegance.",
+        "image": "images/product_potionoval.png"
+    },
+
+
+    {
+        "id": 17,
+        "name": "Tall Potion Pot",
+        "price": 80,
+        "description": "Each Potion is a testament to Melbourne's rich artistic tradition, meticulously formed by skilled hands to embody both beauty and functionality. Whether displayed as a centerpiece or used to adorn a sacred space, Potion exudes an aura of magic and allure, inviting admirers to immerse themselves in its Melbourne-made charm and timeless elegance.",
+        "image": "images/product_potiontall.png"
+    },
+
+
+    {
+        "id": 18,
+        "name": "Sphere Potion Pot",
+        "price": 80,
+        "description": "Each Potion is a testament to Melbourne's rich artistic tradition, meticulously formed by skilled hands to embody both beauty and functionality. Whether displayed as a centerpiece or used to adorn a sacred space, Potion exudes an aura of magic and allure, inviting admirers to immerse themselves in its Melbourne-made charm and timeless elegance.",
+        "image": "images/product_potionsphere.png"
+    },
+
+
+];
+export default productsContent;
